04bd3fe511950a1982f1a5fedb0326843b75734e
2025050005 <inhyang227@gmail.com>
2026-04-29 16:03:09 +0900

    hw5.c

--- p1/main.c
+++ p1/main.c
@@ -6,18 +6,20 @@ int main()
 	int found = 0;
 	scanf("&d", &N);
 
-	for (int a=1; a*900<N; a++) {
-		for (int b=2;  b*750<N; b+=2) {
-			for (int c=1; c*200<N; c++) {
+	for (int a=1; a<=N/900; a++) {
+		for (int b=2;  b<=N/750; b+=2) {
+			for (int c=1; c<=N/200; c++) {
 				if (a*900 + b*750 + c*200 == N){
-				    printf("%d %d %d\n", a, b, c);
-				    found=1;
+		            if (a>c || a>b){
+		                printf("%d %d %d\n", a, b, c);
+				        found=1;
+		            }
 				}
 			}
 		}
 	}
 
-    if (!found){
+    if (found==0){
         printf("none");
     }
     
